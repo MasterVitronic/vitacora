96da97fefb4455b47de4de80ed74b6a8512059eb
Diaz Devera Victor (vitronic) <vitronic2@gmail.com>
2018-08-30 20:32:28 -0400

    Creada la clase config para llevar la configuracion del sitio de forma mas simple

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+            ____                  _     _
+           / ___|_   _  __ _  ___| |__ (_)
+          | |  _| | | |/ _` |/ __| '_ \| |
+          | |_| | |_| | (_| | (__| | | | |
+           \____|\__,_|\__,_|\___|_| |_|_|
+Copyright (c) 2014  Díaz  Víctor  aka  (Máster Vitronic)
+Copyright (c) 2018  Díaz  Víctor  aka  (Máster Vitronic)
+<vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
+*/
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', realpath(dirname(__FILE__)) . DS );
+require_once ROOT . 'lib/class.config.php';
+config::iniciar();
---
+++ lib/class.config.php
@@ -0,0 +1,94 @@
+<?php
+/*
+            ____                  _     _
+           / ___|_   _  __ _  ___| |__ (_)
+          | |  _| | | |/ _` |/ __| '_ \| |
+          | |_| | |_| | (_| | (__| | | | |
+           \____|\__,_|\__,_|\___|_| |_|_|
+Copyright (c) 2014  Díaz  Víctor  aka  (Máster Vitronic)
+Copyright (c) 2018  Díaz  Víctor  aka  (Máster Vitronic)
+<vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
+*/
+
+define('GUACHI_VERSION','1.8');
+
+/*camelCase  :-) */
+class config {
+
+    /**
+     * Instancia para el patrón de diseño singleton (instancia única)
+     * @var object instancia
+     * @access private
+     */
+    private static $instancia = null;
+
+    /**
+     * __construct
+     *
+     * Constructor de la clase
+     *
+     * @access public
+     *
+     */
+    public function __construct() {
+        $this->setConfig();
+    }
+
+    /**
+     * __destruct
+     *
+     * Destructor, destruye automaticamente la clase.
+     *
+     * @access public
+     */
+    public function __destruct() {
+
+    }
+
+    /**
+     * Inicia la instancia de la clase
+     * @return object
+     */
+    public static function iniciar() {
+        if (!self::$instancia instanceof self) {
+            self::$instancia = new self;
+        }
+        return self::$instancia;
+    }
+
+    /**
+     * Método magico __clone
+     */
+    public function __clone() {
+        trigger_error('Operación Invalida:' .
+                ' clonación no permitida', E_USER_ERROR);
+    }
+
+    /**
+     * Método magico __wakeup
+     */
+    public function __wakeup() {
+        trigger_error('Operación Invalida:' .
+                ' deserializar no esta permitido ' .
+                get_class($this) . " Class. ", E_USER_ERROR);
+    }
+
+    /**
+     * metodo setConfig
+     *
+     * @access private
+     */
+    public function setConfig() {
+        $config = parse_ini_file(ROOT . DS . 'guachi.ini', true);
+        foreach ($config as $bloque => $conf) {
+            foreach ($config[$bloque] as $var => $valor) {
+                define(trim($var), trim($valor));
+            }
+        }
+        $header="X-Powered-By: Guachi (Lightweight and very simple web "
+        ."development framework of Vitronic) v".GUACHI_VERSION;
+        header($header);
+    }
+
+}
+
